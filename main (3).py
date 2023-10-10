0fbfe1f807dc7ab8c9ddd255be732862df94b319
Benasir0122127046 <144242524+Benasir0122127046@users.noreply.github.com>
2023-10-10 13:28:36 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,7 @@
+￼year=int(input("enter the year:")) 
+if(year%4000) and /year%100==0) ;
+print(year, "is leap year")
+elif(year%4==0))and(year%100! =0);
+print(year, " is leap year")
+else:
+print(year, "is not leap year")
